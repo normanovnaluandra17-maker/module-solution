d09aea335d5615f1ea1e8a1b15dc925942040600
normanovnaluandra17-maker <normanovnaluandra17@gmail.com>
2026-04-06 20:07:07 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,101 @@
 
   var categories = [];
 
+  var homeHtmlUrl = "snippets/home-snippet.html";
+  var allCategoriesUrl = "https://coursera-jhu-default-random.herokuapp.com/categories.json";
+
+  function buildAndShowHomePage(categories) {
+
+    var randomCategoryShortName = chooseRandomCategory(categories);
+
+    $ajaxUtils.sendGetRequest(
+      homeHtmlUrl,
+      function (homeHtmlSnippet) {
+
+        var homeHtml = insertProperty(
+          homeHtmlSnippet,
+          "randomCategoryShortName",
+          "'" + randomCategoryShortName + "'"
+        );
+
+        document.querySelector("#main-content").innerHTML = homeHtml;
+      },
+      false
+    );
+  }
+
+  function chooseRandomCategory(categories) {
+    var randomIndex = Math.floor(Math.random() * categories.length);
+    return categories[randomIndex].short_name;
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+
+    $ajaxUtils.sendGetRequest(
+      allCategoriesUrl,
+      function (response) {
+
+        categories = response;
+
+        buildAndShowHomePage(categories);
+      }
+    );
+
+  });
+
+})();(function () {
+
+  var categories = [];
+
+  var homeHtmlUrl = "snippets/home-snippet.html";
+  var allCategoriesUrl = "https://coursera-jhu-default-random.herokuapp.com/categories.json";
+
+  function buildAndShowHomePage(categories) {
+
+    var randomCategoryShortName = chooseRandomCategory(categories);
+
+    $ajaxUtils.sendGetRequest(
+      homeHtmlUrl,
+      function (homeHtmlSnippet) {
+
+        var homeHtml = insertProperty(
+          homeHtmlSnippet,
+          "randomCategoryShortName",
+          "'" + randomCategoryShortName + "'"
+        );
+
+        document.querySelector("#main-content").innerHTML = homeHtml;
+      },
+      false
+    );
+  }
+
+  function chooseRandomCategory(categories) {
+    var randomIndex = Math.floor(Math.random() * categories.length);
+    return categories[randomIndex].short_name;
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+
+    $ajaxUtils.sendGetRequest(
+      allCategoriesUrl,
+      function (response) {
+
+        categories = response;
+
+        buildAndShowHomePage(categories);
+      }
+    );
+
+  });
+
+})();(function () {
+
+  var categories = [];
+
+  var homeHtmlUrl = "snippets/home-snippet.html";
+  var allCategoriesUrl = "https://coursera-jhu-default-random.herokuapp.com/categories.json";
+
   function buildAndShowHomePage(categories) {
 
     var randomCategoryShortName = chooseRandomCategory(categories);
